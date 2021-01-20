d8170dbb78255c4e9db06c44f01f7139f953c908
havebear <616749285@qq.com>
2021-01-20 14:55:49 +0800

    chore: 初始化环境，集成vuex、router、jest、ant

---
+++ src/config/api.config.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: 616749285@qq.com
+ * @Date: 2021-01-20 14:38:56
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2021-01-20 14:39:01
+ * @Description:  api配置
+ */
---
+++ src/config/router.config.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: 616749285@qq.com
+ * @Date: 2021-01-20 14:38:44
+ * @LastEditors: 616749285@qq.com
+ * @LastEditTime: 2021-01-20 14:40:24
+ * @Description:  路由配置
+ */
